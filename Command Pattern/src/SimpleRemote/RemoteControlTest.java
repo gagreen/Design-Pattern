4d71acb66918de51b0a50e317a8c350a9f3023b1
minseong <minsang6088@naver.com>
2020-09-19 00:51:04 +0900

    Resolve Quiz

--- Command Pattern/src/SimpleRemote/RemoteControlTest.java
+++ Command Pattern/src/SimpleRemote/RemoteControlTest.java
@@ -6,7 +6,12 @@ public class RemoteControlTest {
 		Light light = new Light(); // 전등 생성
 		LightOnCommand lightOn = new LightOnCommand(light); // light를 조정하는 명령 생성
 		
+		GarageDoor gd = new GarageDoor();
+		GarageDoorOpenCommand doorOpen = new GarageDoorOpenCommand(gd);
+		
 		remote.setCommand(lightOn); // 명령어 전달
 		remote.buttonWasPressed(); // 실행
+		remote.setCommand(doorOpen);
+		remote.buttonWasPressed();
 	}
 }
