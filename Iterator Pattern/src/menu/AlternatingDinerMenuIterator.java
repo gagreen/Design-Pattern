dc420d8bd8abbff3fc574d64ab0a15a5ce9f9291
minseong <minsang6088@naver.com>
2020-11-29 20:08:34 +0900

    Add Iterator Pattern

---
+++ Iterator Pattern/src/menu/AlternatingDinerMenuIterator.java
@@ -0,0 +1,36 @@
+package menu;
+
+import java.util.Iterator;
+import java.util.Calendar;
+
+public class AlternatingDinerMenuIterator implements Iterator {
+	MenuItem[] items;
+	int position;
+	
+	public AlternatingDinerMenuIterator(MenuItem[] items) {
+		this.items = items;
+		Calendar rightNow = Calendar.getInstance();
+		position = rightNow.DAY_OF_WEEK % 2;
+	}
+	
+	@Override
+	public Object next() {
+		MenuItem menuItem = items[position];
+		position += 2;
+		return menuItem;
+	}
+	
+	@Override
+	public boolean hasNext() {
+		if(position >= items.length || items[position] == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	@Override
+	public void remove() {
+		throw new UnsupportedOperationException("remove()는 지원하지 않습니다.");
+	}
+}
--- Iterator Pattern/src/menu/Waitress.java
+++ Iterator Pattern/src/menu/Waitress.java
@@ -6,21 +6,27 @@ import java.util.Iterator;
 public class Waitress {
 	Menu pancakeHouseMenu;
 	Menu dinerMenu;
-	public Waitress(Menu pancakeHouseMenu, Menu dinerMenu) {
+	Menu cafeMenu;
+	
+	public Waitress(Menu pancakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
 		this.pancakeHouseMenu = pancakeHouseMenu;
 		this.dinerMenu = dinerMenu;
+		this.cafeMenu = cafeMenu;
 	}
 	
 	public void printMenu() {
 		Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
 		Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
+		Iterator<MenuItem> cafeIterator = cafeMenu.createIterator();
 		
 		System.out.println("메뉴\n-----\n아침 메뉴");
 		printMenu(pancakeIterator);
-		System.out.println("\n저녁 메뉴");
+		
+		System.out.println("\n점심 메뉴");
 		printMenu(dinerIterator);
 		
-		
+		System.out.println("\n저녁 메뉴");
+		printMenu(cafeIterator);
 	}
 	
 	/* Iterator를 써서 모든 메뉴 항목에 접근하여 내용 출력 */
